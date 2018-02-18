ac3c2d461022ee69f3a16a3aa7ee225774bb6689
Geoff Lee <g.lee@ed.ac.uk>
2018-02-18 06:42:11 +0000

    Remove documentation, make the module do something

--- {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/__init__.py
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/__init__.py
@@ -1,10 +1,22 @@
 """The {{cookiecutter.app_name}} tool"""
+import argparse
+
+VERSION = "{{cookiecutter.version}}"
+DESCRIPTION = """This is the {{cookiecutter.app_name}} tool"""
 
 def main():
     """Main function"""
-    print "This is the {{cookiecutter.app_name}} tool, version {{cookiecutter.version}}"
-    print "Now, make it do something!"
+    args = process_args()
 
+    # Now do stuff!
+    assert args
+
+def process_args(args=False):
+    """Process any commandline arguments"""
+    parser = argparse.ArgumentParser(description=DESCRIPTION,
+                                     version=VERSION)
+    args = parser.parse_args(args)
+    return args
 
 if __name__ == "__main__":
     main()
