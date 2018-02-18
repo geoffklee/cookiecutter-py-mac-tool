2f6afa892720b71e3ab26016c144d86817585b84
Geoff Lee <g.lee@ed.ac.uk>
2018-02-18 06:47:23 +0000

    Run lint natvely rather than shelling out. Test version

--- {{cookiecutter.app_name}}/tests/test_sample.py
+++ {{cookiecutter.app_name}}/tests/test_sample.py
@@ -1,14 +1,41 @@
-# Sample Test running pylint
-
+""" Some sample pytest tests """
 import subprocess
+from ConfigParser import ConfigParser
+from pylint import epylint as lint
+from pytest import raises
+import {{cookiecutter.app_name}} 
+
 
 def test_lint():
-    try:
-        subprocess.check_output(["pylint", "{{cookiecutter.app_name}}/__init__.py"])
-    except subprocess.CalledProcessError as error:
-        print(error.output)
-        print("test_lint() failed.")
-        assert False
+    (out, err) = lint.py_run('{{cookiecutter.app_name}}', return_std="True")
+    result = out.read()
+    if not '10.00/10' in result.split(" "):
+       print "Failed pylint!"
+       print result 
+       assert False
 
- 
+
+def test_version(capsys):
+    """Test that the version number reported by our tool
+       matches what we think it should be
+
+       capsys is a _fixture_:
+         https://docs.pytest.org/en/latest/fixture.html
+    """
+    # Read our supposed version from setup.cfg
+    config = ConfigParser()
+    config.read('setup.cfg')
+    cfg_version = config.get('bumpversion', 'current_version')
+
+    # Call our tool's argument processing function with
+    # a single argumnent of '-v'. This should report the
+    # version number and exit - except, we trap the exit.
+    with raises(SystemExit): # Trap the SystemExit exception
+        {{cookiecutter.app_name}}.process_args(['-v'])
+
+    # Stderr contains our version number
+    out, err = capsys.readouterr()
+
+    # Do they match?
+    assert err.strip() == cfg_version
 
